a271d5d83154b97bdc96def606b11cb003adfe0e
aun1414 <abbasaun1414@gmail.com>
2024-12-08 20:15:23 -0500

    create q learning method and update action method

--- agent.py
+++ agent.py
@@ -1,17 +1,38 @@
 import numpy as np
 
 class Qagent:
-    def __init__(self, env, num_states, phases):
+    def __init__(self,  env, initial_state, phases):
         self.alpha = 0.1
         self.gamma = 0.6
         self.epsilon = 0.1
-        self.q_table = np.zeros((self.num_states, self.phases))
-        self.num_states = num_states
+        self.q_table = {initial_state: np.zeros(phases)}  # Initialize Q-table as a dictionary        
         self.phases = phases
+        self.acc_reward = 0
+
+    def create_action_space(self):
+        self.action_space = list(range(self.phases))  # Action space is a list of indices [0, 1, 2, ...]
 
     def choose_action(self, state):
         if np.random.uniform(0, 1) < self.epsilon:
-            action = np.random.choice(self.phases)
+            self.action = np.random.choice(self.action_space)
         else:
-            action = np.argmax(self.q_table[state])
-        return action
+            self.action = np.argmax(self.q_table[state])
+        return self.action
+
+    def learn(self, next_state, reward, done=False):
+        """Update Q-table with new experience."""
+
+        if next_state not in self.q_table:
+            self.q_table[next_state] = np.zeros(self.phases)
+
+        s = self.state
+        s1 = next_state
+        a = self.action
+        self.q_table[s][a] = self.q_table[s][a] + self.alpha * (
+            reward + self.gamma * max(self.q_table[s1]) - self.q_table[s][a]
+        )
+        self.state = s1
+        self.acc_reward += reward
+
+
+        import numpy as np
