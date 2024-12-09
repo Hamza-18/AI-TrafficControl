c9a97ae9f2d41dcdfdd1abb3d89f66d9f831fb4c
aun1414 <abbasaun1414@gmail.com>
2024-12-08 22:17:20 -0500

    defining wait time thresholds

--- env.py
+++ env.py
@@ -18,23 +18,46 @@ class Enviornment:
         '''get current phase from traci and traffic flow from each lane'''
         state = traci.trafficlight.getRedYellowGreenState(self.tl_id)
         vehicles = traci.vehicle.getIDList()  # Get list of vehicles
-        traffic_flow_level1 = 0
-        traffic_flow_level2 = 0
-        traffic_flow_level3 = 0
-        self.wait_time_level1 = 0
-        self.wait_time_level2 = 0
-        self.wait_time_level3 = 0
+        traffic_flow_top_right = 0
+        traffic_flow_bot_left = 0
+        traffic_flow_bot = 0
+        self.wait_traffic_flow_top_right = 0
+        self.wait_traffic_flow_bot_left = 0
+        self.wait_traffic_flow_bot = 0
         for veh_id in vehicles:
             lane_id = traci.vehicle.getRoadID(veh_id)
             vehicle_wait_time = traci.vehicle.getAccumulatedWaitingTime(veh_id)
             if lane_id == "-E0":
-                traffic_flow_level1 += 1
-                self.wait_time_level1 += vehicle_wait_time
+                traffic_flow_top_right += 1
+                self.wait_traffic_flow_top_right += vehicle_wait_time
             elif lane_id == "E0":
-                traffic_flow_level2 += 1
-                self.wait_time_level2 += vehicle_wait_time
+                traffic_flow_bot_left += 1
+                self.wait_traffic_flow_bot_left += vehicle_wait_time
             elif lane_id == "E1":
-                traffic_flow_level3 += 1
-                self.wait_time_level3 += vehicle_wait_time
+                traffic_flow_bot += 1
+                self.wait_traffic_flow_bot += vehicle_wait_time
+        
+        if self.wait_traffic_flow_top_right > 50:
+            traffic_flow_top_right = "high"
+        elif self.wait_traffic_flow_top_right > 20 and self.wait_traffic_flow_top_right < 50:
+            traffic_flow_top_right = "med"
+        else:
+            traffic_flow_top_right = "low"
+        
+        if self.wait_traffic_flow_bot_left > 50:
+            traffic_flow_bot_left = "high"
+        elif self.wait_traffic_flow_bot_left > 20 and self.wait_traffic_flow_bot_left < 50:
+            traffic_flow_bot_left = "med"
+        else:
+            traffic_flow_bot_left = "low"
+
+        if self.wait_traffic_flow_bot > 50:
+            traffic_flow_bot = "high"
+        elif self.wait_traffic_flow_bot > 20 and self.wait_traffic_flow_bot < 50:
+            traffic_flow_bot = "med"
+        else:
+            traffic_flow_bot = "low"
+
+        return state, traffic_flow_top_right, traffic_flow_bot_left, traffic_flow_bot
 
         
